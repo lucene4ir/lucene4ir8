a83e271f4aade9546c286844afc3614e1094c3ce
ABDULAZIZ ALQATAN <47518534+ABDULAZIZALQATAN@users.noreply.github.com>
2019-07-28 10:52:05 +0100

    Enhance Code Schema
    1- Split Retrieval Experiment From Retrievability Experiment for a given b value
    2- add ability to run both experiments for a given b value

--- src/main/java/lucene4ir/RunExperimentsRetrievabilityCalculatorApp.java
+++ src/main/java/lucene4ir/RunExperimentsRetrievabilityCalculatorApp.java
@@ -8,46 +8,45 @@ public class RunExperimentsRetrievabilityCalculatorApp {
     private String retirievalParamsFile = "params/temp_retrieval_params.xml",
             retrievalOutDir = "out/G/300K/B/",
             retrievabilityParamsFile = "params/temp_retrievability_calculator_params.xml";
-    private XMLTextParser retParser,
-                          rcParser;
-    private RetrievalApp retApp ;
-    private RetrievabilityCalculatorApp rcApp ;
 
-
-    private void init()
+    private void runRetrievalExperiment(String b)
     {
-        retParser = new XMLTextParser(retirievalParamsFile);
-        rcParser = new XMLTextParser(retrievabilityParamsFile);
-    }
-
-    private void runAllExperiment (String b)
-    {
+        // Run RetrievalApp Experiment for given B Value
         String outFileName;
 
+        XMLTextParser parser = new XMLTextParser(retirievalParamsFile);
         outFileName = retrievalOutDir + "result" + b + ".res";
-        retParser.setTagValue("b", b );
-        retParser.setTagValue("resultFile",outFileName);
-        retParser.save();
-        retApp = new RetrievalApp(retirievalParamsFile);
+        parser.setTagValue("b", b );
+        parser.setTagValue("resultFile",outFileName);
+        parser.save();
+        RetrievalApp retApp = new RetrievalApp(retirievalParamsFile);
         retApp.processQueryFile();
-        runRCExperiment(b);
-    }
+    } // End Function
 
     private void runRCExperiment (String b)
     {
+        // Run Retrievability Calculator Experiments for given B Value
         String outFileName;
-        outFileName = retrievalOutDir + "result" + b + ".res";
-        rcParser.setTagValue("resFile",outFileName);
-        outFileName = retrievalOutDir + "RCResults" + b + ".ret";
-        rcParser.setTagValue("retFile",outFileName);
-        rcParser.save();
-        rcApp = new RetrievabilityCalculatorApp(retrievabilityParamsFile);
-        rcApp.calculate();
-    }
 
-    public RunExperimentsRetrievabilityCalculatorApp(){
-       init();
-    }
+        XMLTextParser parser = new XMLTextParser(retrievabilityParamsFile);
+        outFileName = retrievalOutDir + "result" + b + ".res";
+        parser.setTagValue("resFile",outFileName);
+        outFileName = retrievalOutDir + "RCResults" + b + ".ret";
+        parser.setTagValue("retFile",outFileName);
+        parser.save();
+        RetrievabilityCalculatorApp rcApp = new RetrievabilityCalculatorApp(retrievabilityParamsFile);
+        rcApp.calculate();
+    } // End Function
+
+
+    private void runWholeExperiment (String b)
+    {
+        // Run RetrievalApp & Retrievability Calculator Experiments for given B Value
+        runRetrievalExperiment(b);
+        runRCExperiment(b);
+    } // End Function
+
+
 
     public static void main(String[] args) {
 	// write your code here

730f8c79ece397d18e516d810ba109e69877d416
ABDULAZIZ ALQATAN <47518534+ABDULAZIZALQATAN@users.noreply.github.com>
2019-12-07 07:20:02 +0000

    Update StatisticsIndex.java
    Statistical Index Update ( Update Many Functions )

--- src/main/java/lucene4ir/Stats/StatisticsIndex.java
+++ src/main/java/lucene4ir/Stats/StatisticsIndex.java
@@ -13,6 +13,7 @@ Created By : Abdulaziz AlQatan - 21/07/2019
 package lucene4ir.Stats;
 
 import lucene4ir.ExampleStatsApp;
+import lucene4ir.Lucene4IRConstants;
 import lucene4ir.utils.TokenAnalyzerMaker;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
@@ -39,7 +40,7 @@ public class StatisticsIndex {
    */
     public int uniqueTermsCtr ,emptyTermsCtr , allTermsCtr , docsCtr;
     public double avgDocLength;
-    String outDir , indexName , fldName;
+    String outDir , indexName , fldName , indexesFolder;
     PrintWriter pr;
     boolean screenOutput;
     int maxdoc;
@@ -75,7 +76,6 @@ public class StatisticsIndex {
 
         System.out.println(outText);
     } // End Function
-
 
     private void iterateField(String fldName) throws IOException {
         // Iterate through the given Field (fldName)
@@ -159,7 +159,7 @@ public class StatisticsIndex {
     private void printFieldList () throws Exception
     {
         ExampleStatsApp ex = new ExampleStatsApp();
-        ex.indexName = indexName;
+        ex.indexName = indexesFolder + indexName;
         ex.openReader();
         ex.fieldsList();
     } // End Function
@@ -171,6 +171,14 @@ public class StatisticsIndex {
             pr = new PrintWriter(outFileName);
     }
 
+    private void close() throws Exception
+    {
+        if (pr != null)
+            pr.close();
+        if (reader != null)
+            reader.close();
+        System.out.println("Done Successfully ");
+    }
     private void printLine (String line)
     {
         if (screenOutput)
@@ -181,26 +189,40 @@ public class StatisticsIndex {
 
     private void openReader()
     {
+        String tempIndexName;
         try {
-            reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexName)));
+            if (indexesFolder.isEmpty())
+                tempIndexName = indexName;
+            else
+                tempIndexName = indexesFolder + indexName;
+            reader = DirectoryReader.open(FSDirectory.open(Paths.get(tempIndexName)));
         } catch (IOException e) {
             e.printStackTrace();
         }
     } // End Function
 
-    private void printTermList(boolean withFrequency) throws Exception
+    private void printTermList(int docid, boolean withFrequency) throws Exception
     {
         String line ;
         Terms trs;
         TermsEnum it;
         BytesRef term;
+        int ctr = 0 , start  , end;
         openWriter("TermList");
-      //  line = String.format("Index Name : %s\nFieldName : %s",indexName , fldName);
-      //  printLine(line);
+
         openReader();
-        if (maxdoc == 0 )
-            maxdoc = reader.maxDoc();
-        for (int i = 0 ; i < maxdoc ; i++)
+        if (docid < 0 )
+        {
+            end = reader.maxDoc();
+            start = 0;
+        }
+        else
+        {
+            start = docid;
+            end = docid + 1;
+        }
+
+        for (int i = start ; i < end ; i++)
         {
             trs = reader.getTermVector(i,fldName);
          //   line = "Document " + i + "\nTermList : ";
@@ -213,10 +235,12 @@ public class StatisticsIndex {
                 if (withFrequency)
                     line += " " + it.totalTermFreq();
                 printLine(line);
+                ctr++;
                 } // End while
         } // End For
+        line = "Total Terms = " + ctr;
+        printLine(line);
     } // End Function
-
 
     private void printTermLeavesCount () throws Exception
     {
@@ -234,33 +258,34 @@ public class StatisticsIndex {
         printLine("Total Terms : " + totalTerms);
     }
 
-/*private void newTermCount() throws Exception
-{
-    openReader();
-    reader.document(0).getField(fldName).tokenStream(new StandardAnalyzer() , ts);
-}*/
-
-    private void printTermCount() throws Exception
+    private void printTermCount(int docid) throws Exception
     {
         String line ;
         Terms trs;
-        long totalTerms = 0 , currentTermsCount;
-        ExampleStatsApp ex = new ExampleStatsApp();
-        ex.indexName = indexName;
+        long totalTerms = 0 , currentTermsCount ;
+        int start , end;
+
         openWriter("TermCount");
+        openReader();
 
-        line = String.format("Index Name : %s\nFieldName : %s",indexName , fldName);
+        line = String.format("Index Name : %s\nFieldName : %s\nDocID    Length",indexName , fldName);
         printLine(line);
-        ex.openReader();
-
-        if (maxdoc == 0 )
-            maxdoc = ex.reader.maxDoc();
-        for (int i = 0 ; i < maxdoc ; i++)
+       if (docid < 0 )
+       {
+           start = 0;
+           end = reader.maxDoc();
+       }
+        else
+       {
+           start = docid;
+           end = start + 1;
+       }
+        for (int i = start ; i < end ; i++)
         {
-           trs = ex.reader.getTermVector(i,fldName);
-           currentTermsCount = trs.getSumTotalTermFreq();
-         //  line = "Document " + i + " TermCount : " + currentTermsCount;
-         //   printLine(line);
+           trs = reader.getTermVector(i,fldName);
+           currentTermsCount = trs.size();
+            line = " " + i + "          " + currentTermsCount;
+            printLine(line);
             totalTerms += currentTermsCount;
         } // End For
         line = "Total Terms : " + totalTerms + "\n" +
@@ -272,28 +297,28 @@ public class StatisticsIndex {
     {
         String line ;
         ExampleStatsApp ex = new ExampleStatsApp();
-        ex.indexName = indexName;
-        ex.openReader();
-        screenOutput = true;
+        openReader();
         line =    "Index : " + indexName
-                + "\nLeaves Count : " + ex.reader.leaves().size();
+                + "\nLeaves Count : " + reader.leaves().size();
         printLine(line);
     }
 
 
-private void  checkAnalyzer (String indexType) throws Exception
+private void  checkAnalyzer (String all , String indexType) throws Exception
 {
-    String all = "AAR CORP reports earnings for Qtr to Nov 30 1987/01/011987 The New York Times COMPANY REPORTS Statistics Top/News/Business Company Reports Corporations The New York Times AAR CORP reports earnings for Qtr to Nov 30 LEAD: *3*** COMPANY REPORTS ** *3*AAR CORP (NYSE) Qtr to Nov 30 1986 1985 Sales 75,907,000 61,040,000 Net inc 3,953,000 2,858,000 Share earns .38 .32 Shares outst 10,479,000 9,069,000 6mo sales 142,283,000 114,876,000 Net inc 7,054,000 5,300,000 Share earns .71 .59 Shares outst 9,932,000 9,066,000 LEAD: *3*** COMPANY REPORTS ** *3*AAR CORP (NYSE) Qtr to Nov 30 1986 1985 Sales 75,907,000 61,040,000 Net inc 3,953,000 2,858,000 Share earns .38 .32 Shares outst 10,479,000 9,069,000 6mo sales 142,283,000 114,876,000 Net inc 7,054,000 5,300,000 Share earns .71 .59 Shares outst 9,932,000 9,066,000 *3*** COMPANY REPORTS ** *3*AAR CORP (NYSE) Qtr to Nov 30 1986 1985 Sales 75,907,000 61,040,000 Net inc 3,953,000 2,858,000 Share earns .38 .32 Shares outst 10,479,000 9,069,000 6mo sales 142,283,000 114,876,000 Net inc 7,054,000 5,300,000 Share earns .71 .59 Shares outst 9,932,000 9,066,000 The 1985 share earnings and shares outstanding are adjusted for the 3-for- 2 stock split in February 1986. The company said the 1986 shares outstanding reflects the pro rata effect of the issuance of 1.35 million shares in public offering in August 1986.  The New York Times 1987/01/01",
-            biTokenFilterFile = "params/index/TokenFilterFile_Bigram.xml",
+    String  biTokenFilterFile = "params/index/TokenFilterFile_Bigram.xml",
             uniTokenFilterFile = "params/index/TokenFilterFile_Unigram.xml" ,
             combinedTokenFilterFile = "params/index/TokenFilterFile_Combinedgram.xml",
-            inputFilter = "",line;
+            inputFilter = "";
     TokenAnalyzerMaker tam = new TokenAnalyzerMaker();
     Analyzer an;
     TokenStream ts;
     CharTermAttribute term;
     int termsCount = 0;
 
+    System.out.println("Start Checking " + indexType);
+    System.out.println("input : '" + all + "'");
+    System.out.println("------------------");
     switch (indexType)
     {
         case "Unigram" :
@@ -309,7 +334,6 @@ private void  checkAnalyzer (String indexType) throws Exception
     an = tam.createAnalyzer(inputFilter);
     ts =  an.tokenStream(fldName,all);
     ts.reset();
-    screenOutput = true;
 
     while (ts.incrementToken())
     {
@@ -318,54 +342,62 @@ private void  checkAnalyzer (String indexType) throws Exception
         termsCount++;
     }
 
-    printLine(indexType + " Index has " + termsCount + " terms");
+    printLine(indexType + " Index has " + termsCount +
+            " terms\n----------------------------------------");
 } // End Function
 
-    private void printDocLength (int docID) throws Exception
-    {
-        openReader();
-      String temp = reader.document(docID).get("raw").toString();
-    }
     private void printDocCount ()
     {
         openReader();
         System.out.println("Total Document Count : " + reader.maxDoc());
     }
+
+    private void printIndexSet () throws Exception {
+                String indexList[] =
+                {"AquaintBigramIndex","AquaintCombinedIndex", "AquaintUnigramIndex",
+                "Core17BigramIndex","Core17CombinedIndex","Core17UnigramIndex"};
+        for (int i = 0 ; i < indexList.length ; i++)
+        {
+            indexName = indexList[i];
+            printTermCount(-1);
+            close();
+        }
+    }
     public static void main(String[] args) {
         /*
       Small :  smallFieldedIndex smallCombinedIndex smallBigramIndex smallUnigramIndex testIndex biraw
       Single : SingleDocument30WordsBigramIndex SingleDocument30WordsCombinedIndex SingleDocument30WordsUnigramIndex SingleDocument30WordsFieldedIndex
-      Corpus:  Core17BigramIndex Core17CombinedIndex Core17UnigramIndex Core17FieldedIndex
-       AquaintsmallFieldedIndex AquaintsmallUnigramIndex AquaintsmallBigramIndex AquaintsmallCombinedIndex
-       Corpus:  AquaintBigramIndex AquaintCombinedIndex AquaintUnigramIndex AquaintFieldedIndex
-         */
+      Corpus:
+        AquaintUnigramIndex AquaintBigramIndex AquaintCombinedIndex AquaintFieldedIndex
+        Core17UnigramIndex Core17BigramIndex Core17CombinedIndex Core17FieldedIndex */
 
-        String indexesFolder = "C:\\Users\\kkb19103\\Desktop\\My Files 07-08-2019\\BiasMeasurementExperiments\\Indexes\\";
-        StatisticsIndex sts = new StatisticsIndex();
-         // C:/Users/kkb19103/Desktop/My Files 07-08-2019/LUCENE/anserini-master/lucene-index.core18.pos+docvectors+rawdocs
-        // oneDocCombinedIndex - oneDocUnigramIndex
-        sts.indexName =  "oneDocCombinedIndex";
-        sts.fldName = "raw";
+       StatisticsIndex sts = new StatisticsIndex();
+      /*  AquaintUnigramIndex AquaintBigramIndex AquaintCombinedIndex AquaintFieldedIndex
+        Core17UnigramIndex Core17BigramIndex Core17CombinedIndex Core17FieldedIndex */
+
+        sts.indexesFolder = "C:\\Users\\kkb19103\\Desktop\\My Files 07-08-2019\\BiasMeasurementExperiments\\Indexes\\";
+        sts.indexName =  "Core17CombinedIndex";
+        sts.fldName = Lucene4IRConstants.FIELD_RAW;
         sts.outDir = "C:\\Users\\kkb19103\\Desktop\\CheckTerms\\";
-
         sts.screenOutput = true;
         sts.maxdoc = 0;
         try {
-         //   sts.checkAnalyzer("Unigram");
-        //    sts.checkAnalyzer("Bigram");
-        //    sts.checkAnalyzer("Combined");
+            String all = "This is my house and I am going to the kitchen ";
+            sts.checkAnalyzer(all,"Unigram");
+            sts.checkAnalyzer(all,"Bigram");
+            sts.checkAnalyzer(all,"Combined");
 
           //  sts.printDocLength(0);
 
-            sts.printTermList(true);
-        //   sts.printTermCount();
-          //  sts.printDocCount();
-
+           // sts.printTermList(1,false);
+          // sts.printTermCount(-1);
+           // sts.printDocCount();
+           // sts.printDocLength(Lucene4IRConstants.FIELD_RAW);
           //  sts.printLeavesCount();
          //  sts.printTermLeavesCount();
          //  sts.printFieldList();
-            if (!sts.screenOutput)
-                sts.pr.close();
+         //  sts.printIndexSet();
+           sts.close();
         } catch (Exception e) {
             e.printStackTrace();
         }

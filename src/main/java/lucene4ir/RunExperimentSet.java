6c6eabe73271484d5e914797dd5b518bb9d76c6f
ABDULAZIZ ALQATAN <47518534+ABDULAZIZALQATAN@users.noreply.github.com>
2020-03-02 07:26:06 +0000

    Updates 02-03-2020
    Experiment2.XML : Change CSVFile to CSVPath to refer to a path of three csv files (Res - Ret - Per )
    CSVParser :
    Remove Many Lines ( Use Key - Value Combination for all csv files simply ) Each csv file has its own csvParser simply and directly
    RunExperiment :
    Handle multiple CSV Files ( Res - Ret - Per ) + the new CSV Line Format

--- src/main/java/lucene4ir/RunExperimentSet.java
+++ src/main/java/lucene4ir/RunExperimentSet.java
@@ -22,8 +22,8 @@ public class RunExperimentSet {
             uniFilter = "params/index/p.tokenFilterFile_Unigram.xml" ,
             combinedFilter = "params/index/p.tokenFilterFile_Combinedgram.xml" ,
             tokenFilterFile = combinedFilter,*/
-            defaultCSVKey,filePrefix;
-    private CSVParser csvPaser;
+            defaultCSVKey , filePrefix;
+    private CSVParser csvPaserPer , csvPaserRes , csvPaserRet;
     private  HashMap<String, Double> initMap;
     private String getCorpus (String indexName)
     {
@@ -75,8 +75,13 @@ public class RunExperimentSet {
          Sample OutputDir
          out\Core17\UnigramIndex\50\C1000\CombinedgramFilter
          */
-        String qryCount = "" , indexFolder = "" , C = "", corpus , csvFile ,
-        outPutFolder = "C:\\Users\\kkb19103\\Desktop\\My Files 07-08-2019\\BiasMeasurementExperiments\\WAPO 26-02-2020";
+        String qryCount = "" , indexFolder = "" , C = "", corpus , outPutFolder,csvPath;
+
+        if (p.mainDir.isEmpty())
+            outPutFolder = "C:\\Users\\kkb19103\\Desktop\\My Files 07-08-2019\\BiasMeasurementExperiments\\WAPO 26-02-2020";
+        else
+            outPutFolder = p.mainDir;
+
 
         if (validRetrievability() &&
                 initMap == null &&
@@ -85,7 +90,6 @@ public class RunExperimentSet {
             cloneMap();
         C = "C" + p.maxResults;
         if (validRetrievability())
-      //  if (p.maxResults.equals("100"))
             qryCount = "300K";
         else
             qryCount = "50";
@@ -103,25 +107,46 @@ public class RunExperimentSet {
             indexFolder = "FieldedIndex";
 
         //   Sample Output
-        //   out\Core17\UnigramIndex\50\C1000\CombinedgramFilter
-        filePrefix = String.format("%s\\%s-%s-%s-%s-%s-" ,
+        //   \ExperimentFolder\WA-BM25-BI-50-C1000-b0.6.res
+        filePrefix = String.format("%s\\ExperimentFolder\\%s-%s-%s-%s-%s-" ,
                 outPutFolder , corpus.substring(0,2) ,
                 p.model , indexFolder.substring(0 ,1) + "I"
                 , qryCount , C);
 
-        if (p.csvFile.isEmpty())
-            csvFile = "C:\\Users\\kkb19103\\Desktop\\My Files 07-08-2019\\BiasMeasurementExperiments\\out.csv";
-        else
-            csvFile = p.csvFile;
-        csvPaser = new CSVParser();
-        csvPaser.readFromFile(csvFile);
-
-        /*CSV Key
+         /*CSV Key
         corpus - indexType - qryFilter - qryCount - model - maxResults - RetrievabilityB - RetrievalCoefficient (B , mu , c) */
-        defaultCSVKey = String.format("%s,%s,%s,%s,%s,%s,",
+        defaultCSVKey = String.format("%s,%s,%s,%s,%s,%s",
                 corpus,indexFolder , "combinedQuery" , qryCount , p.model , p.maxResults);
+
         p.indexName = outPutFolder + "\\Indexes\\" + p.indexName;
-    }
+        if (p.csvPath.isEmpty())
+            csvPath = "C:\\Users\\kkb19103\\Desktop\\My Files 07-08-2019\\BiasMeasurementExperiments\\CSV";
+        else
+            csvPath = p.csvPath;
+        switch (p.exType)
+        {
+            case "All":
+                csvPaserRes = new CSVParser(csvPath,"Res");
+                csvPaserRet = new CSVParser(csvPath,"Ret");
+                break;
+            case "AllRC":
+            case "Cumulative":
+            case "Gravity":
+                csvPaserRet = new CSVParser(csvPath,"Ret");
+                break;
+            case "Retrieval":
+                csvPaserRes = new CSVParser(csvPath,"Res");
+                break;
+            case "Performance":
+                csvPaserPer = new CSVParser(csvPath,"Per");
+                break;
+            case "ReadSts":
+                csvPaserPer = new CSVParser(csvPath,"Per");
+                csvPaserRes = new CSVParser(csvPath,"Res");
+                csvPaserRet = new CSVParser(csvPath,"Ret");
+        } // End Switch
+    } // End Function fillAutoParameters
+
     private void readParamsFromFile(String paramFile) throws Exception
     {
         // Read All Parameters and check that all of them are true
@@ -144,16 +169,41 @@ public class RunExperimentSet {
         parser.save();
     } // End Function
 
+    private String getCofficientCaption()
+    {
+        String result = "";
+        if (p.model.equals("BM25"))
+            result = "b";
+        else if (p.model.equals("PL2"))
+            result = "c";
+        else if (p.model.equals("LMD"))
+            result = "mu";
+        return result;
+    }
+
+    private String getPerformanceFile (String coefficient)
+    {
+        // /Per/WA-BM25-BI-50-C1000-b0.1.trec
+        String result = filePrefix.replace("ExperimentFolder","Per");
+        result += getCofficientCaption() + coefficient + ".trec";
+        return result;
+    } // End Function
+
     private String getResFile (String coefficient)
     {
-       return filePrefix + "result" + coefficient + ".res";
-    }
+       String result = filePrefix.replace("ExperimentFolder","Res");
+       result += getCofficientCaption() + coefficient + ".res";
+       return result;
+    } // End Function
 
     private String getRetFile (String b , String coefficient)
     {
-        // WA-BM25-BI-300K-C100-0.5-RCResults0.1.ret
-        return String.format("%s%s-RCResults%s.ret",filePrefix,b,coefficient);
-    }
+        // /Ret/WA-PL2-FI-300K-C100-gb0-c5.ret
+
+        String result = filePrefix.replace("ExperimentFolder","Ret");
+        result =  String.format("%sgb%s-%s%s.ret",result,b,getCofficientCaption(), coefficient);
+        return result;
+    } // End Function
 
     private String runRetrievalStatistics (String b)
     {
@@ -172,22 +222,17 @@ public class RunExperimentSet {
         return String.format("%1.6f,%d,%1.4f",sts.G , sts.zeroDocCtr , sts.sum );
     }
 
-    private String runRetrievalExperiment(String b)
+    private void runRetrievalExperiment(String coefficient)
     {
         // Run RetrievalApp Experiment for given B Value
-        String outFileName , param;
+        String outFileName , param , value ;
         FieldedRetrievalApp fRetApp;
         RetrievalApp retApp;
 
         XMLTextParser parser = new XMLTextParser(p.retrievalParamsFile);
-        outFileName = filePrefix + "result" + b + ".res";
-        if (p.model.equals("BM25"))
-            param = "b";
-        else if (p.model.equals("PL2"))
-            param = "c";
-        else
-            param = "mu";
-        parser.setTagValue(param, b );
+        outFileName = getResFile(coefficient);
+        param = getCofficientCaption();
+        parser.setTagValue(param, coefficient );
         parser.setTagValue("resultFile",outFileName);
         parser.setTagValue("queryFile",p.queryFile);
         parser.setTagValue("indexName",p.indexName);
@@ -204,13 +249,15 @@ public class RunExperimentSet {
             retApp = new RetrievalApp(p.retrievalParamsFile);
             retApp.processQueryFile();
         } // End Else
-        return runRetrievalStatistics(b);
+        value = runRetrievalStatistics(coefficient);
+        csvPaserRes.addCSVLineToMap(getResKey(coefficient),value);
+
     } // End Function
 
-    private String runRCExperiment (String coefficient , String b)
+    private void runRCExperiment (String coefficient , String b)
     {
         // Run Retrievability Calculator Experiments for given B Value
-        String outFileName ;
+        String outFileName , value , retKey;
         XMLTextParser parser = new XMLTextParser(p.retrievabilityParamsFile);
         outFileName = getResFile(coefficient);
         parser.setTagValue("resFile",outFileName);
@@ -223,55 +270,56 @@ public class RunExperimentSet {
         RetrievabilityCalculatorApp rcApp = new RetrievabilityCalculatorApp(p.retrievabilityParamsFile);
         rcApp.setMap(initMap);
         rcApp.calculate();
-        return String.format("%1.6f,%d,%1.4f",rcApp.G , rcApp.zeroRCtr, rcApp.rSum);
+        retKey = getRetKey(coefficient, b);
+        value = String.format("%1.6f,%d,%1.4f",rcApp.G , rcApp.zeroRCtr, rcApp.rSum);
+        csvPaserRet.addCSVLineToMap(retKey,value);
         // return runRCStatistics(coefficient,b);
     } // End Function
-    private String getPerformanceValues(String b) throws Exception
+    private void runPerformanceExperiment(String coefficient) throws Exception
     {
         String inFile ,
                 line,
-                result = "",
+                value = "",
                 keys[] = {"map","bpref","P10"};
         BufferedReader br;
         int i = 0;
-        inFile = filePrefix + "trec" + b + ".trec";
+        inFile = getPerformanceFile(coefficient);
         br = new BufferedReader(new FileReader(inFile));
 
         while ((line = br.readLine()) != null && i < keys.length )
             if (line.startsWith(keys[i]))
             {
-                result += "," + line.split("\t",3)[2];
+                value += "," + line.split("\t",3)[2];
                 i++;
             } // End if
-        result = result.replaceFirst(",","");
-        return result;
+        value = value.replaceFirst(",","");
+        csvPaserRes.addCSVLineToMap(getResKey(coefficient),value);
     } // End Function
-
-    private String getNewKey (String retB , String b)
-    {
-        return defaultCSVKey + retB + "," + b;
-    }
 
     private boolean validRetrievability()
     {
         return p.maxResults.equals("100");
     }
+    private String getResKey(String coefficient)
+    {
+        return defaultCSVKey + "," + coefficient ;
+    }
+
+    private String getRetKey(String coefficient , String b)
+    {
+        return String.format("%s,%s,%s" ,defaultCSVKey , b , coefficient);
+    }
+
     private void runALLRC (String coefficient)
     {
-        String result , b;
+        String  b;
         // Calculation
         if (validRetrievability())
         {
             b = "0";
-            result = runRCExperiment(coefficient,b);
-            // *** Statistics ***
-            csvPaser.setRet(result);
-            csvPaser.addCSVLineToMap();
-
+            runRCExperiment(coefficient,b);
             b = "0.5";
-            result = runRCExperiment(coefficient,b);
-            csvPaser.setKey(getNewKey(b,coefficient));
-            csvPaser.setRet(result);
+            runRCExperiment(coefficient,b);
         } // End if (validRetrievability())
 }
 
@@ -280,17 +328,17 @@ private String getCurrentTime() {
         return LocalDateTime.now().format(dtf);
     }
 
-    private String[] getbRange()
+    private String[] getCoefficientRange()
     {
         /*
         Get Coefficient Values based on input model
          */
         String[]
-                // BM25Set = {"0.1", "0.2","0.3","0.4","0.5","0.6","0.7","0.8","0.9","1"},
-               //  PL2Set = {"0.1", "0.5", "1", "5", "10", "15", "20", "50"},
+                 BM25Set = {"0.1", "0.2","0.3","0.4","0.5","0.6","0.7","0.8","0.9","1"},
+                 PL2Set = {"0.1", "0.5", "1", "5", "10", "15", "20", "50"},
                  LMDSet = {"100","200","300","400","500","600","700","800","900","1000","5000"},
-                 BM25Set = {"0.1", "0.3","0.5","0.7","0.9"},
-                 PL2Set = {"0.1", "1", "10", "20", "50"},
+               //  BM25Set = {"0.1", "0.3","0.5","0.7","0.9"},
+               //  PL2Set = {"0.1", "1", "10", "20", "50"},
                //  LMDSet = {"100","200","300","400","500","600","700","800"},
 
                 // Complement
@@ -306,68 +354,80 @@ private String getCurrentTime() {
     private void processExperimentSet () throws Exception
     {
         // This function is used to process the whole experiment Set
-         String coefficient , retB  = "0", bashLines = ""  ;
-         String bRange[] = getbRange();
-         for (int i = 0; i < bRange.length; i++) {
-             coefficient = bRange[i];
-         if (p.exType.equals("Gravity"))
-             retB = "0.5";
-        csvPaser.newLine(getNewKey(retB,coefficient));
+         String coefficient , b , bashLines = "" , value , retKey  ;
+         String coefficientRange[] = getCoefficientRange();
+         boolean updateRes = false , updateRet = false , updatePer = false;
+         for (int i = 0; i < coefficientRange.length; i++) {
+             coefficient = coefficientRange[i];
 
         switch (p.exType)
             {
                 case "All":
-                    csvPaser.setRes(runRetrievalExperiment(coefficient));
+                    runRetrievalExperiment(coefficient);
                     // Checking ValidRetrievability Inside These Functions
                     runALLRC(coefficient);
                     bashLines += getBashLine(coefficient);
+                    updateRes = true;
+                    updateRet = true;
                     break;
                 case "AllRC":
                     runALLRC(coefficient);
+                    updateRet = true;
                     break;
                 case "Cumulative":
+                    if (validRetrievability())
+                        runRCExperiment(coefficient,"0");
+                    updateRet = true;
+                    break;
                 case "Gravity":
                     if (validRetrievability())
-                        csvPaser.setRet(runRCExperiment(coefficient,retB));
+                        runRCExperiment(coefficient,"0.5");
+                    updateRet = true;
                     break;
 
                 case "Retrieval":
-                    csvPaser.setRes(runRetrievalExperiment(coefficient));
+                    runRetrievalExperiment(coefficient);
                     bashLines += getBashLine(coefficient);
+                    updateRes = true;
                     break;
                 case "Performance" :
                     if (!validRetrievability())
-                        csvPaser.setPerformance(getPerformanceValues(coefficient));
+                        runPerformanceExperiment(coefficient);
+                    updatePer = true;
                     break;
                 case "ReadSts":
                     if (validRetrievability())
                         {
-                        retB = "0";
-                        csvPaser.setKey(getNewKey(retB,coefficient));
-                        csvPaser.setRes(runRetrievalStatistics(coefficient));
-                        csvPaser.setRet(runRCStatistics(coefficient,retB));
-                        csvPaser.addCSVLineToMap();
-                        retB = "0.5";
-                        csvPaser.setKey(getNewKey(retB,coefficient));
-                        csvPaser.setRes(runRetrievalStatistics(coefficient));
-                        csvPaser.setRet(runRCStatistics(coefficient,retB));
+                        value = runRetrievalStatistics(coefficient);
+                        csvPaserRes.addCSVLineToMap(getResKey(coefficient),value);
+                        updateRes = true;
+                        b = "0";
+                        retKey = getRetKey(coefficient,b);
+                        value = runRCStatistics(coefficient,b);
+                        csvPaserRet.addCSVLineToMap(retKey,value);
+                        b = "0.5";
+                        retKey = getRetKey(coefficient,b);
+                        value = runRCStatistics(coefficient,b);
+                        csvPaserRet.addCSVLineToMap(retKey,value);
+                        updateRet = true;
                       } // End if
                    else
                     {
-                        retB = "0";
-                        csvPaser.setKey(getNewKey(retB,coefficient));
-                        csvPaser.setRes(runRetrievalStatistics(coefficient));
-                        csvPaser.setPerformance(getPerformanceValues(coefficient));
+                        runPerformanceExperiment(coefficient);
+                        updatePer = true;
                     } // End Else
             } // End Switch
-            csvPaser.addCSVLineToMap();
-
         } // End For
 
         if (!bashLines.isEmpty())
-            printOutput(filePrefix + "bash.sh" , bashLines);
-        csvPaser.updateCSVFile();
-
+            printOutput(filePrefix.replace("ExperimentFolder","Bash") + "-bash.sh"
+                      , bashLines);
+        if (updateRes)
+            csvPaserRes.updateCSVFile();
+        if (updateRet)
+            csvPaserRet.updateCSVFile();
+        if (updatePer)
+            csvPaserPer.updateCSVFile();
     } // End Function
 
     private void runExperimentFile (String fileName)
@@ -398,11 +458,11 @@ private String getCurrentTime() {
               /* "AquaintBigramIndex","AquaintCombinedIndex","AquaintUnigramIndex" , "AquaintFieldedIndex",
                "Core17UnigramIndex","Core17BigramIndex","Core17CombinedIndex"  , "Core17FieldedIndex"*/
              //  "WAPOUnigramIndex","WAPOBigramIndex","WAPOCombinedIndex"  , "WAPOFieldedIndex"
-               "WAPOFieldedIndex"
+               "WAPOUnigramIndex"
        };
        String maxResults[] = {"100","1000"};
-       String models[] = {"BM25","PL2"};
-      //   String models[] = {"LMD"};
+      // String models[] = {"BM25","PL2" , "LMD"};
+       String models[] = {"BM25"};
        for (int m = 0 ; m < models.length ; m++)
            for ( int i = 0 ; i < indexNames.length ; i++)
                for (int j=0 ; j < maxResults.length ; j++ )
@@ -428,11 +488,9 @@ private String getCurrentTime() {
     public static void main(String[] args)
         {
         // write your code here
-            String sourceFile = "params\\BMExperimentSets\\Experiment2.xml",
-                    result;
+        String sourceFile = "params\\BMExperimentSets\\Experiment2.xml";
+
         RunExperimentSet re = new RunExperimentSet();
-
-
         re.runCalculatedList();
         // re.runExperimentFile(sourceFile);
 
@@ -447,5 +505,6 @@ class ExperimentSetParams  {
             model,
             retrievalParamsFile,
             retrievabilityParamsFile,
-            csvFile;
+            csvPath,
+            mainDir;
 }
--- src/main/java/lucene4ir/parse/CSVParser.java
+++ src/main/java/lucene4ir/parse/CSVParser.java
@@ -19,52 +19,45 @@ public class CSVParser {
  */
     // CSV Key Fields
     public HashMap<String, String> csvMap;
-    public CSVLine csvLine;
-    String csvFile;
+   // public CSVLine csvLine;
+    // Res - Ret - Per
+    String csvType , csvFile;
 
     private final String separator = ",";
 
-    public CSVParser() {
+    public CSVParser(String csvPath , String inCsvType) {
         csvMap = new HashMap<String, String>();
-        csvLine = new CSVLine();
+        csvType = inCsvType.toLowerCase();
+        csvFile = csvPath + "\\" + inCsvType + ".csv";
+        readFromFile(csvFile);
     }
 
-    private String extractValue(String csvValues, int start ,  int paramCount) {
-       /*
-       Extract Specific subset Of Values from a given csvValues of one line
-        if csvValues is Empty - return empty separated values
-        else
-        Extract the Existing values Separated By Comma
-        */
-        String result = "", parts[];
-        int end;
-
-        if (csvValues.isEmpty()) {
-            for (int i = 1; i < paramCount; i++)
-                result += " ,";
-            result += " ";
-        } // End if
-        else {
-            end = start + paramCount;
-            parts = csvValues.split(separator, end + 1);
-            for (int i = start ; i < end ; i++)
-                result += separator + parts[i] ;
-            result = result.replaceFirst(separator,"");
-        } // End Else
-        return result;
-    }
+    private String getHeader ()
+    {
+        String keyHeader = "corpus,indexType,qryFilter,qryCount,model,maxResults,RetrievalCoefficient,";
+        String result = "";
+        switch (csvType)
+        {
+            case "res":
+                result = keyHeader + "resLineCount,resDocCount,limitedQueries<max";
+                break;
+            case "ret":
+                keyHeader = "corpus,indexType,qryFilter,qryCount,model,maxResults,RetrievabilityB,RetrievalCoefficient,";
+                result = keyHeader + "G,ZeroRValuesCount,rSum";
+                break;
+            case "per":
+                result = keyHeader + "MAP,Bref,P10,RBP0.6,RBP0.8";
+                break;
+        } // End Switch
+      return result + "\n";
+    } // End Function getHeader
 
     public void updateCSVFile() {
         String line;
-        TreeMap<String, String> sortedMap = new TreeMap<String, String>(csvMap);
-        // header
-        line = "corpus,indexType,qryFilter,qryCount,model,maxResults,RetrievabilityB,RetrievalCoefficient," +
-                "G,ZeroRValuesCount,rSum," +
-                "resLineCount,resDocCount,limitedQueries<max," +
-                "MAP,Bref,P10\n";
         try {
             PrintWriter pr = new PrintWriter(csvFile);
-            Iterator it = sortedMap.entrySet().iterator();
+            Iterator it = csvMap.entrySet().iterator();
+            line = getHeader();
             pr.write(line);
             while (it.hasNext()) {
                 Map.Entry entry = (Map.Entry) it.next();
@@ -77,38 +70,8 @@ public class CSVParser {
         } // End Catch
     }
 
-    public String addCSVLineToMap() {
-        String value = "" , newKey;
-        int  retCount = 3 , resCount = 3 , performanceCount = 3 ;
-        int start;
-
-        newKey = csvLine.getKey();
-        if (newKey == null || newKey.isEmpty())
-            System.out.println("CSV Key is Empty");
-        else {
-            if (csvMap.containsKey(newKey))
-                value = csvMap.get(newKey);
-
-            // Retrievability Cumulative
-            start = 0;
-            if (csvLine.getRet().isEmpty())
-                csvLine.setRet(extractValue(value,start,retCount));
-
-            // Res
-            start += retCount;
-            if (csvLine.getRes().isEmpty())
-                csvLine.setRes(extractValue(value,start,resCount));
-
-            start +=resCount;
-            if (csvLine.getPerformance().isEmpty())
-                csvLine.setPerformance(extractValue(value,start,performanceCount));
-            value = csvLine.getAllValues();
-
-            csvMap.put(newKey, value);
-            value = newKey + separator + value;
-            System.out.println("Added CSV Line : " + value);
-        } // End Else
-        return value;
+    public void addCSVLineToMap(String inKey , String inValue) {
+        csvMap.put(inKey,inValue);
     }
 
     public void readFromFile(String file) {
@@ -116,11 +79,13 @@ public class CSVParser {
         Read All CSV Lines from The input File
         Split Lines into (Key , Value ) and put them in CSV HashMap
          */
-
         String line, parts[], key, value;
-        int keyCount = 8;
+        int keyCount;
+        if (csvType.equals("ret"))
+            keyCount = 8;
+        else
+            keyCount = 7;
 
-        csvFile = file;
         if (!new File(file).exists())
             return;
         try {
@@ -137,65 +102,4 @@ public class CSVParser {
             e.printStackTrace();
         } // End Catch
     } // End Function readFromFile
-
-    public void setPerformance (String performance)
-    { csvLine.setPerformance(performance); }
-
-    public void setRes(String res)
-    { csvLine.setRes(res); }
-
-    public void setRet(String ret)
-    { csvLine.setRet(ret); }
-
-    public void setKey (String key)
-    { csvLine.setKey(key); }
-
-    public String getKey ()
-    { return csvLine.getKey(); }
-
-    public void newLine(String key)
-    { // Append NewCSV Line
-        csvLine.setKey(key);
-        csvLine.resetValues();
-    }
-}
-
-class CSVLine {
-    // public String res,retCG,retGG , performance, key;
-    // Parameter Groups in CSV Line
-    public String key = "" , ret = "" , res = "" , performance = "";
-
-    public void resetValues ()
-    {
-        setRes("");
-        setRet("");
-        setPerformance("");
-    }
-
-    public String getAllValues()
-    {
-        return String.format("%s,%s,%s", ret , res , performance );
-    }
-
-
-    public void setPerformance(String performance) { this.performance = performance; }
-    public String getPerformance() { return performance; }
-
-    public void setRes(String res) { this.res = res; }
-    public String getRes() {
-        return res;
-    }
-
-    public void setRet(String ret) { this.ret = ret; }
-    public String getRet() {
-        return ret;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-    public String getKey()
-    {
-        return key;
-    }
 }
